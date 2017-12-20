d8181a4d8d5f65611ff618090737c709448cdb2e
Анциферов Ян <Anci_oq88>
2017-12-20 16:21:03 +0200

    add ticks

--- Form1.cs
+++ Form1.cs
@@ -51,20 +51,20 @@ namespace Clock
         {
             using (var g = Graphics.FromImage(img))
             {
-               
                 g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
                 g.Clear(Color.Gray);
 
                 g.TranslateTransform(xCenter, yCenter);
 
-                g.FillEllipse(Brushes.Red, -10, -10, 20, 20);
+                DrawBody(g);
 
-                DrawTicks(g, TypeTick.Twelve);
+                DrawTicks(g, TypeTick.All);
 
                 DrawNumbers(g, TypeNumbers.Quadric);
 
                 DrawClock(g);
             }
+            pictureBox1.Invalidate();
         }
 
         private void DrawTicks(Graphics g, TypeTick type)
@@ -79,30 +79,30 @@ namespace Clock
             Pen blackPen = new Pen(Brushes.Black, 8);
             for (int i = 0; i < 4; i++)
             {
-                g.DrawLine(new Pen(Brushes.Black, 8), 0, -Radius + 20, 0, -Radius - 2);
+                g.DrawLine(new Pen(Brushes.Black, Radius / 30), 0, -Radius + (Radius / 8), 0, -Radius - 2);
                 g.RotateTransform(90);
             }
 
             if(type == TypeTick.Twelve || type == TypeTick.All)
             {
-                blackPen = new Pen(Brushes.Black, 5);
+                blackPen = new Pen(Brushes.Black, Radius / 50);
                 for (int j = 0; j < 12; j++)
                 {
                    
                     g.RotateTransform(30);
-                    g.DrawLine(blackPen, 0, -Radius + 20, 0, -Radius);
+                    g.DrawLine(blackPen, 0, -Radius + (Radius / 10), 0, -Radius);
                 }
             }
 
             if(type == TypeTick.All)
             {
-                blackPen = new Pen(Brushes.Black, 3);
+                blackPen = new Pen(Brushes.Black, Radius / 70);
                 g.RotateTransform(6);
                 for (int j = 0; j < 60; j++)
                 {
 
                     g.RotateTransform(6);
-                    g.DrawLine(blackPen, 0, -Radius + 10, 0, -Radius);
+                    g.DrawLine(blackPen, 0, -Radius + (Radius / 20), 0, -Radius);
                 }
 
             }
@@ -116,24 +116,54 @@ namespace Clock
             if (type == TypeNumbers.None)
                 return;
 
-            g.ResetTransform();
-            g.TranslateTransform(xCenter, yCenter);
+            //g.ResetTransform();
+            //g.TranslateTransform(xCenter, yCenter);
 
-            if (type == TypeNumbers.Quadric)
-            {
-                g.DrawString(12.ToString(), new Font("Arial", Radius / 10, FontStyle.Bold), Brushes.Black, -20, -Radius + 30);
+            //if (type == TypeNumbers.Quadric)
+            //{
+            //    g.DrawString(12.ToString(), new Font("Arial", Radius / 10, FontStyle.Bold), Brushes.Black, -20, -Radius + 30);
 
-            }
+            //}
         }
 
         private void DrawClock(Graphics g)
         {
 
-            int degree = (360 / 60) * DateTime.Now.Second;
+            g.FillEllipse(Brushes.Black, -15, -15, 30, 30);
+
+           
+
+            int degree = (360 / 60) * DateTime.Now.Minute;
+
+            g.RotateTransform(degree);
+            g.DrawLine(new Pen(Brushes.Black, 7), 0, -Radius + 50, 0, 20);
+            g.RotateTransform(-degree);
+
+
+            degree = (360 / 60) * DateTime.Now.Hour;
+
+            g.RotateTransform(degree);
+            g.DrawLine(new Pen(Brushes.Black, 10), 0, -Radius + 75, 0, 20);
+            g.RotateTransform(-degree);
+
+            g.FillEllipse(Brushes.Red, -10, -10, 20, 20);
+
+             degree = (360 / 60) * DateTime.Now.Second;
 
             g.RotateTransform(degree);
             g.DrawLine(new Pen(Brushes.Red, 2), 0, -Radius + 15, 0, 20);
-            g.RotateTransform(0);
+            g.RotateTransform(-degree);
+
+         
+
+         
+        }
+
+
+        private void DrawBody(Graphics g)
+        {
+            g.FillEllipse(Brushes.Aqua, -Radius, -Radius, (Radius * 2), (Radius * 2));
+            g.DrawEllipse(new Pen(Brushes.Black, Radius / 60), -Radius, -Radius, (Radius * 2), (Radius * 2));
         }
 
         private void Form1_Resize(object sender, EventArgs e)
