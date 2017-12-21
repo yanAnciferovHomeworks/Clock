90e23f57d0e0a206e9adfe0e053098f2465e8177
Yan <o.loo2016@gmail.com>
2017-12-22 01:56:34 +0200

    add test

---
+++ Test/Test/Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.Ticks = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.NumbersType = new System.Windows.Forms.ComboBox();
+            this.clock1 = new AnalogClock.Clock();
+            this.button8 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.NumbersType);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.Ticks);
+            this.groupBox1.Controls.Add(this.groupBox2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(254, 333);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Стили";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 104);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Секундная стрелка";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(6, 75);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(128, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Минутная стрелка";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(6, 46);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(128, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Часовая стрелка";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(152, 75);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(84, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Фон";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(152, 46);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(84, 23);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Кольцо";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(152, 104);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(84, 23);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "Тело";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(152, 17);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(84, 23);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "Засечки";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button8);
+            this.groupBox2.Controls.Add(this.button7);
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Controls.Add(this.button6);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.button5);
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Controls.Add(this.button4);
+            this.groupBox2.Location = new System.Drawing.Point(6, 191);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(242, 136);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Цвета";
+            // 
+            // Ticks
+            // 
+            this.Ticks.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.Ticks.FormattingEnabled = true;
+            this.Ticks.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.Ticks.Location = new System.Drawing.Point(90, 152);
+            this.Ticks.Name = "Ticks";
+            this.Ticks.Size = new System.Drawing.Size(152, 21);
+            this.Ticks.TabIndex = 8;
+            this.Ticks.SelectedIndexChanged += new System.EventHandler(this.Ticks_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 155);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Стиль засечек";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 128);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Стиль чисел";
+            // 
+            // NumbersType
+            // 
+            this.NumbersType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.NumbersType.FormattingEnabled = true;
+            this.NumbersType.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.NumbersType.Location = new System.Drawing.Point(90, 125);
+            this.NumbersType.Name = "NumbersType";
+            this.NumbersType.Size = new System.Drawing.Size(152, 21);
+            this.NumbersType.TabIndex = 10;
+            this.NumbersType.SelectedIndexChanged += new System.EventHandler(this.NumbersType_SelectedIndexChanged);
+            // 
+            // clock1
+            // 
+            this.clock1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.clock1.BackColor = System.Drawing.SystemColors.Control;
+            this.clock1.BackGroundColor = System.Drawing.SystemColors.Highlight;
+            this.clock1.BodyColor = System.Drawing.Color.BlueViolet;
+            this.clock1.CircleBodyColor = System.Drawing.Color.Black;
+            this.clock1.ForeColor = System.Drawing.Color.Coral;
+            this.clock1.HourArrowColor = System.Drawing.Color.Black;
+            this.clock1.Location = new System.Drawing.Point(291, 21);
+            this.clock1.MinuteArrowColor = System.Drawing.Color.Black;
+            this.clock1.Name = "clock1";
+            this.clock1.NumbersColor = System.Drawing.Color.Black;
+            this.clock1.NumberType = AnalogClock.TypeNumbers.None;
+            this.clock1.SecondArrowColor = System.Drawing.Color.SaddleBrown;
+            this.clock1.Size = new System.Drawing.Size(348, 324);
+            this.clock1.TabIndex = 0;
+            this.clock1.TickColor = System.Drawing.Color.Black;
+            this.clock1.TickType = AnalogClock.TypeTick.All;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(6, 19);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(128, 23);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "Числа";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(651, 357);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.clock1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AnalogClock.Clock clock1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox Ticks;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox NumbersType;
+        private System.Windows.Forms.Button button8;
+    }
+}
+
---
+++ Test/Test/Form1.cs
@@ -0,0 +1,92 @@
+﻿using AnalogClock;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test
+{
+    public partial class Form1 : Form
+    {
+        ColorDialog colorDialog;
+        public Form1()
+        {
+            InitializeComponent();
+            colorDialog = new ColorDialog();
+
+            Ticks.Items.Add(TypeTick.None);
+            Ticks.Items.Add(TypeTick.Quadric);
+            Ticks.Items.Add(TypeTick.Twelve);
+            Ticks.Items.Add(TypeTick.All);
+            Ticks.SelectedIndex = 3;
+
+            NumbersType.Items.Add(TypeNumbers.All);
+            NumbersType.Items.Add(TypeNumbers.Quadric);
+            NumbersType.Items.Add(TypeNumbers.None);
+            NumbersType.SelectedIndex = 0;
+        }
+
+        private Color GetNewColor()
+        {
+
+            colorDialog.ShowDialog();
+            
+            return colorDialog.Color;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            clock1.HourArrowColor = GetNewColor();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            clock1.MinuteArrowColor = GetNewColor();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            clock1.SecondArrowColor = GetNewColor();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            clock1.TickColor = GetNewColor();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            clock1.CircleBodyColor = GetNewColor();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            clock1.BackGroundColor = GetNewColor();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            clock1.BodyColor = GetNewColor();
+        }
+
+        private void Ticks_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            clock1.TickType = (TypeTick)Ticks.SelectedItem;
+        }
+
+        private void NumbersType_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            clock1.NumberType = (TypeNumbers)NumbersType.SelectedItem;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            clock1.NumbersColor = GetNewColor();
+        }
+    }
+}
